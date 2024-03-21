d0279d9273544959585fd39d3329eb6a728e9969
Em-Costello <emilyevecostello@gmail.com>
2024-03-21 14:52:38 +0000

    Added more documentation

--- membership_file_reader.py
+++ membership_file_reader.py
@@ -3,8 +3,8 @@ import re
 import h5py
 import numpy as np
 
-membership_directory = "/cosma8/data/dp004/flamingo/Runs/L1000N1800/HYDRO_FIDUCIAL/SOAP/membership_0077/"
-pattern = "membership_0077\.\d+\.hdf5"
+membership_directory = "/cosma8/data/dp004/flamingo/Runs/L1000N1800/HYDRO_FIDUCIAL/SOAP/membership_0077/" # Location of membership files
+pattern = "membership_0077\.\d+\.hdf5" # Membership file format
 
 # Find all membership_files matching the pattern
 membership_files = [f for f in os.listdir(membership_directory) if re.match(pattern, f)]
@@ -17,17 +17,18 @@ def extract_number(filename):
 membership_files.sort(key=extract_number)
 
 print(membership_files)
+
 # Initialize an empty list to store data arrays
-BH_GroupNumber_arrays = []
+Property_arrays = []
 
 # Loop through each file and read its data into the list
 for file_name in membership_files:
     with h5py.File(os.path.join(membership_directory, file_name), 'r') as f:
-        BH_GroupNumber = f['PartType5/GroupNr_all'][:]  
-        BH_GroupNumber_arrays.append(BH_GroupNumber)
+        Property = f['name_of_property'][:]  # Change to required quantity name 
+        Property_arrays.append(Property)
 
 print('Combining data...')
 # Concatenate all arrays into one
-BH_GroupNumber_all = np.concatenate(BH_GroupNumber_arrays, axis=0)
-print(len(BH_GroupNumber_all))
+Property_combined = np.concatenate(Property_arrays, axis=0)
+print(len(Property_combined))
 # Now combined_data contains all data from the membership_files in a single array
